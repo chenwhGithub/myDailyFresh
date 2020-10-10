86d1abfbddfe7145929c38db81f0cbc397ceaf50
wenhchen <wenhui.chen@nokia-sbell.com>
2020-10-10 14:58:44 +0800

    homepage click register/login button, jump to html page

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
+class RegisterView(View):
+
+    def get(self, request):
+        ''' 首页点击注册按钮，跳转到 register.html 页面 '''
+        return render(request, 'register.html')
+
+
+class LoginView(View):
+
+    def get(self, request):
+        ''' 首页点击登录按钮，跳转到 login.html 页面 '''
+        return render(request, 'login.html')
