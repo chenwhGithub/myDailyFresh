836dc4867c3439806c1d355aabf91ebd917dd05c
wenhchen <wenhui.chen@nokia-sbell.com>
2020-10-21 14:41:25 +0800

    update and delete cart info

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import InfoView
+from .views import InfoView, UpdateView, DeleteView
 
 app_name = 'cart'
 urlpatterns = [
     path('', InfoView.as_view(), name='info'),
+    path('update', UpdateView.as_view(), name='update'),
+    path('delete', DeleteView.as_view(), name='delete'),
 ]
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import JsonResponse
 from django_redis import get_redis_connection
 from utils.mixin import LoginRequiredMixin
 from goods.models import GoodsSKU
@@ -33,3 +34,40 @@ class InfoView(LoginRequiredMixin , View):
             'skus': skus,
         }
         return render(request, 'cart.html', context)
+
+
+class UpdateView(View):
+    ''' 更新购物车信息 '''
+    def post(self, request):
+        user = request.user
+        if not user.is_authenticated:
+            return JsonResponse({'status': 1, "errmsg": '请先登录'})
+
+        sku_id = request.POST.get('sku_id')
+        count = request.POST.get('count')
+        sku = GoodsSKU.objects.get(id=sku_id)
+        count = int(count)
+        if count > sku.stock:
+            return JsonResponse({'status':2, 'errmsg':"库存不够"})
+
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%d'%user.id
+        conn.hset(cart_key, sku_id, count)
+
+        return JsonResponse({'status':0, 'count':count})
+
+
+class DeleteView(View):
+    def post(self, request):
+        ''' 删除购物车信息 '''
+        user = request.user
+        if not user.is_authenticated:
+            return JsonResponse({'status': 1, "errmsg": '请先登录'})
+
+        sku_id = request.POST.get('sku_id')
+
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%d'%user.id
+        conn.hdel(cart_key, sku_id)
+
+        return JsonResponse({'status':0})
